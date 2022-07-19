83511867793dde5f3f1c1170e898e06e59b7a007
AsianSiam <contact@asiansiam.com>
2022-07-19 13:12:46 +0200

    correction - C00 Ex00-06

---
+++ piscine/C00/ex04/ft_is_negative_main.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+void    ft_is_negative(int n);
+
+int main(int n)
+{
+    ft_is_negative(-44);
+    ft_is_negative(55);
+    return 0;
+}
--- piscine/C00/ex06/ft_print_comb2.c
+++ piscine/C00/ex06/ft_print_comb2.c
@@ -1,29 +1,41 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mbabel <marvin@42lausanne.ch>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/14 20:55:04 by mbabel            #+#    #+#             */
-/*   Updated: 2022/07/14 21:02:23 by mbabel           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
+#include <stdbool.h>
 
-void ft_print_comb2(void);
+void	ft_putchar (char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_write_comb(int a, int b, int last)
+{
+	ft_putchar(48 + a / 10);
+	ft_putchar(48 + a % 10);
+	ft_putchar(' ');
+	ft_putchar(48 + b / 10);
+	ft_putchar(48 + b % 10);
+	if (last)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}	
+}
+
+void	ft_print_comb2(void)
 {
 	int	a;
 	int	b;
+	int last;
 
-	a = '0';
-	while( a <= 99)
+	a = 0;
+	while(a <= 99)
 	{
 		b = a + 1;
 		while (b <= 99)
 		{
-
+			last =! (a == 98 && b == 99);
+			ft_write_comb(a, b, last);
+			b++;
 		}
+		a++;
 	}
 }
